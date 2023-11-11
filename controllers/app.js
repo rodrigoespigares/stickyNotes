71e312dc238602fd3290c7f76c7bcea11730cd87
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2023-11-11 04:40:23 +0100

    se mueven pares e impares

--- controllers/app.js
+++ controllers/app.js
@@ -1,17 +1,27 @@
-var div, myNotes;
+var div;
+var myNotes;
 var count = 0;
-window.onload = () => {
+var ratonEncima =false;
+var posX;
+var posY;
+var notes;
+
+window.onload = () => 
+{
     div = new viewNote();
     myNotes = new ListNotes();
-
+    
     document.getElementById("btnNote").addEventListener("click", () => {
         div.create(count);
         saveBtn(count);
         count++
     });
+
+    
 }
 
-function saveBtn(count) {
+function saveBtn(count) 
+{
     let btn = document.getElementById("save"+count)
     btn.addEventListener("click", () => {
         let tit = document.getElementById("inputTitle"+count).value;
@@ -25,5 +35,38 @@ function saveBtn(count) {
         let myNote = new Note(tit, txt, hour);
         myNotes.addNotes(myNote);
         div.successful(count,myNote.getTitle(), myNote.getText(), myNote.getHour());
+        moveNote();
+    });
+}
+function moveNote(){
+    let notes = document.getElementsByClassName("on");
+
+    for (let i = 0; i < notes.length; i++) 
+    {
+        notes[i].ratonEncima = false;
+        
+        notes[i].addEventListener("click", function (e) {
+            this.ratonEncima = !this.ratonEncima;
+            if (this.ratonEncima) {
+                
+                let rect = this.getBoundingClientRect();
+                this.posX = e.clientX - rect.left;
+                this.posY = e.clientY - rect.top;
+            }
+        });
+    }
+
+    document.addEventListener("mousemove", function (e) {
+        targetElement = e.target;
+
+        console.log(targetElement.ratonEncima);
+
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].ratonEncima) {
+                
+                notes[i].style.top = e.clientY - notes[i].posY + "px";
+                notes[i].style.left = e.clientX - notes[i].posX + "px";
+            }
+        }
     });
 }
--- views/view.js
+++ views/view.js
@@ -1,51 +1,66 @@
 class viewNote
 {
     myCreate = {
-        input : document.createElement("input"),
         h3 : document.createElement("h3"),
-        textarea : document.createElement("textarea"),
         p : document.createElement("p"),
         h6 : document.createElement("h6"),
-        btnSave : document.createElement("button"),
     }
     create(id)
     {
         let div = document.createElement("div");
         div.className = "note";
         div.id = id;
-        this.myCreate.input.className = "title";
-        this.myCreate.input.id = "inputTitle"+id;
-        this.myCreate.textarea.className = "text";
-        this.myCreate.textarea.id = "inputText"+id; 
-        this.myCreate.btnSave.id = "save"+id;
-        this.myCreate.btnSave.textContent="Save";
-        div.appendChild(this.myCreate.input);
-        div.appendChild(this.myCreate.textarea);
-        div.appendChild(this.myCreate.btnSave);
+        let input = document.createElement("input");
+        input.className = "title";
+        input.id = "inputTitle"+id;
+        let textarea = document.createElement("textarea");
+        textarea.className = "text";
+        textarea.id = "inputText"+id; 
+        let btnSave = document.createElement("button");
+        btnSave.id = "save"+id;
+        btnSave.textContent="Save";
+        div.appendChild(input);
+        div.appendChild(textarea);
+        div.appendChild(btnSave);
         document.body.appendChild(div);
     }
-    successful(id, title, text, hour) {
+    successful(id, title, text, hour) 
+    {
         let div = document.getElementById(id)
+        div.classList="note on"
         let input = document.getElementById("inputTitle"+id);
         let textarea = document.getElementById("inputText"+id);
-        let button = document.getElementById("save"+id); // Agregué esta línea para obtener el botón
-        this.myCreate.h3.textContent = title;
-        this.myCreate.p.textContent = text;
-        this.myCreate.h6.textContent = hour;
-        
-        // Verifica si los elementos existen antes de intentar eliminarlos
+        let button = document.getElementById("save"+id);
+        let h3 = document.createElement("h3");
+        h3.textContent = title;
+        let p = document.createElement("p");
+        p.textContent = text;
+        let h6 = document.createElement("h6");
+        h6.textContent = hour;
         if (input) div.removeChild(input);
         if (textarea) div.removeChild(textarea);
         if (button) div.removeChild(button);
+        h3.className = "title";
+        h3.id = "title"+id;
+        p.className = "text";
+        p.id = "text"+id;
+        h6.className = "hour";
+        h6.id = "hour"+id;
+        let move = document.createElement("button");
+        move.id="move"+id;
+        move.className="move";
+        move.textContent="Move";
+        div.appendChild(h3);
+        div.appendChild(p);
+        div.appendChild(h6);
+        div.appendChild(move);
+    }
+    move(note, deltaX, deltaY) {
+        // Mover la nota sumando las diferencias a las coordenadas actuales
+        let currentLeft = parseInt(window.getComputedStyle(note).left);
+        let currentTop = parseInt(window.getComputedStyle(note).top);
 
-        this.myCreate.h3.className = "title";
-        this.myCreate.h3.id = "title";
-        this.myCreate.p.className = "text";
-        this.myCreate.p.id = "text";
-        this.myCreate.h6.className = "hour";
-        this.myCreate.h6.id = "hour";
-        div.appendChild(this.myCreate.h3);
-        div.appendChild(this.myCreate.p);
-        div.appendChild(this.myCreate.h6);
+        note.style.left = currentLeft + deltaX + 'px';
+        note.style.top = currentTop + deltaY + 'px';
     }
 }
